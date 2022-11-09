ba413df66ab56adcd362d02941eea46f790a4e16
Selim Fazylov <selim.fazylov1@gmail.com>
2022-11-10 01:22:45 +0200

    module 5 hw

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-function getPromise (): Promise<(string | number)[]> {
+function getPromise (): Promise<Array<string | number>> {
     return new Promise((resolve) => {
     resolve(['Text', 50]);
     });
@@ -8,6 +8,7 @@ getPromise ().then((data) => {
     console.log(data);
 });
 
+// ===================================================
 type AllType = {
   name: string;
   position: number;
@@ -15,11 +16,33 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+function compare (top: Pick<AllType, 'name' | 'color'>, bottom: Pick<AllType, 'position' | 'weight'>): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
+  }
+};
+
+// ====================================================
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign({}, objA, objB);
+}
+
+// ===================================================
+interface IComponent {
+  title: string;
+}
+
+class Component<T> {
+  constructor (public props:T) {
+
+  }
+}
+
+class Page extends Component<IComponent> {
+  pageInfo (): void {
+    console.log(this.props.title);
   }
 }
