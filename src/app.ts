f78e6665afa27e8a629ad0c87a9149cc1a6ab7ed
Selim Fazylov <selim.fazylov1@gmail.com>
2022-11-11 00:53:58 +0200

    module5 added

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,35 @@
-function getPromise (): Promise<Array<string | number>> {
-    return new Promise((resolve) => {
-    resolve(['Text', 50]);
-    });
+// function myDecorator(target: Function) {
+//     const newConstructor: Function = function (name: string) {
+//         this.name = "Hello, " + name;
+//     }
+//     return newConstructor;
+// }
+
+function Logger(logString: string) {
+  return function(constructor: Function) {
+    console.log(logString);
+    console.log(constructor.prototype);
+  };
 }
 
-getPromise ().then((data) => {
-    console.log(data);
-});
+@Logger('decorator')
+class Class {
+    private owner: string = "";
+    constructor(public name: string, public age: number) {
+        this.name;
+        this.age;
+    };
 
-// ===================================================
-type AllType = {
-  name: string;
-  position: number;
-  color: string;
-  weight: number
+    setOwner() {
+        this.owner = this.name + "," + this.age.toString();
+        
+    };
+
+    showOwner() {
+        console.log(this.owner);
+    }
 }
 
-function compare (top: Pick<AllType, 'name' | 'color'>, bottom: Pick<AllType, 'position' | 'weight'>): AllType {
-  return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-  }
-};
+const selim = new Class('Selim', 28);
 
-// ====================================================
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign({}, objA, objB);
-}
-
-// ===================================================
-interface IComponent {
-  title: string;
-}
-
-class Component<T> {
-  constructor (public props:T) {
-
-  }
-}
-
-class Page extends Component<IComponent> {
-  pageInfo (): void {
-    console.log(this.props.title);
-  }
-}
+console.log(selim)
